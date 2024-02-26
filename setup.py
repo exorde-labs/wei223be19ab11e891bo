79abf0cd146bb2d3034627300e4e7a5994d391ca
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2024-02-26 15:35:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="wei223be19ab11e891bo",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     install_requires=[
         "python-dotenv",
